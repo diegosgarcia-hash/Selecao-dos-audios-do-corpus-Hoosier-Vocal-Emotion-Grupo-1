25c4b7a93e811283eae8888c00cf4682d0b8639b
Diego Silva Garcia <diego.sgarcia@ufrpe.br>
2026-03-18 14:34:48 -0300

    Update main_script.js

--- data_includes/main_script.js
+++ data_includes/main_script.js
@@ -1,7 +1,7 @@
 PennController.ResetPrefix(null);
 PennController.DebugOff();
 
-Sequence("Terms", "Participants","Instructions", randomize("Experiment"), SendResults(), "Final");
+Sequence("Terms","Instructions", randomize("Experiment"), SendResults(), "Final");
 
 Header(
     defaultText
@@ -32,47 +32,41 @@ Header(
         ,
         newText(
         `<p style="text-align: justify;">
-            Você foi selecionado(a) para participar da pesquisa <strong>"ANÁLISE ACÚSTICA DA RAIVA E TRISTEZA NO CORPUS HOOSIER VOCAL EMOTION COLLECTION EM RELAÇÃO A PADRÕES ACÚSTICOS DESCRITOS NA LITERATURA"</strong>, sob a responsabilidade do pesquisador Diego Silva Garcia e orientador Ebson Wikerson.<br><br>
-            Esta pesquisa tem como objetivo aacvaliar os padrões acústicos presentes nas emoções raiva e tristeza do corpus Hoosier Vocal Emotion Collection e o que a literatura descreve. Sua participação é voluntária e será realizada por meio deste formulário eletrônico, no qual você ouvirá cada áudios do corpus e indicará se os aprova ou não para a análise acústica. Todas as respostas serão anônimas e utilizadas exclusivamente para fins acadêmicos, a partir dos dados obtidos serão selecionados os 15 áudios mais votados para seguir para a análise acústica.<br><br>
-            Ao continuar e enviar suas respostas, você concorda que os dados coletados sejam utilizados na pesquisa e na elaboração do TCC. Para quaisquer dúvidas, você pode entrar em contato com o pesquisador responsável:</p>`
+            Você foi selecionado(a) para participar da pesquisa <strong>"ANÁLISE DOS PADRÕES ACÚSTICOS DA RAIVA E TRISTEZA NO CORPUS HOOSIER VOCAL EMOTION COLLECTION"</strong>, sob a responsabilidade do pesquisador Diego Silva Garcia e orientador Ebson Wilkerson.<br><br>
+            Este formulário é parte de um projeto que objetiva avaliar os padrões acústicos presentes nas emoções raiva e tristeza do corpus Hoosier Vocal Emotion Collection. Sua participação é voluntária e será realizada por meio deste formulário eletrônico, no qual você ouvirá cada áudio do corpus e indicará se os aprova ou não para a análise acústica.<br><br>
+            O seu papel durante o teste será ouvir atentamente cada gravação e selecionar os áudios que considerar mais agradáveis, naturais ou familiares, de acordo com sua percepção auditiva. Não há respostas certas ou erradas, buscamos compreender quais amostras vocais são percebidas como mais adequadas para representar as emoções analisadas.<br><br>
+            Como sabido, as emoções desempenham um papel fundamental na comunicação, influenciando não apenas a forma como as mensagens são transmitidas, mas também como são interpretadas pelos interlocutores. Elas contribuem para a construção de sentidos, modulam a entonação, o ritmo e a intensidade da fala, e auxiliam na identificação de estados afetivos e intenções comunicativas. No contexto da comunicação vocal, características acústicas como frequência fundamental, intensidade e variações prosódicas são elementos essenciais para a expressão e o reconhecimento das emoções, tornando sua análise relevante para estudos nas áreas de Linguística, Fonoaudiologia, Psicologia e Tecnologia da Fala.<br><br>
+            Ao continuar e enviar suas respostas, você concorda que os dados coletados sejam utilizados na pesquisa e na elaboração do TCC. Para quaisquer dúvidas, você pode entrar em contato com o pesquisador responsável:
+        </p>`
             )
         ,
         newText("<p style='text-align:center;'><strong>E-mail: diego.sgarcia@ufrpe.br</strong></p>")
         ,
         newText("<p></p>")
         ,
-        newButton("CONCORDO E CONTINUE")
-            .css({"margin-bottom": "50px"})  // espaço de 50px abaixo do botão
-        )
-    
-    newTrial("Participants",
-        newText("<p>Bem-Vindos!</p>")
-        ,
-        newText("<p>Antes de iniciar, escreva o seu <strong>nome completo</strong> abaixo.</p>")
-        ,
         newTextInput("Nome")
         ,
         newText("<p></p>")
         ,
-        newButton("Iniciar")
+        newButton("CONCORDO E CONTINUE")
         ,
-        newVar("NOME") //Retorna os dados de "Nome" para "NOME"
+        newVar("NOME")
             .global()
             .set( getTextInput("Nome") )
+        ,
     )
 
 .log( "NOME" , getVar("NOME") )
 
     newTrial("Instructions",
          
-        newText("<p><strong>INSTRUÇÕES:</strong></p>")
-        .center()
-        ,
-        newText('<p style="text-align:center;">Assista com atenção ao vídeo a seguir para compreender como o teste irá funcionar.</p>')
+        newText('<p style="text-align: justify;">Para uma melhor compreensão de como o teste irá funcionar, assista ao vídeo abaixo com atenção. Após compreender completamente o conteúdo, clique no botão “INICIAR O TESTE” para iniciar o teste.</p>')
         ,  
+        newText("<p></p>")
+        ,
         newHtml("yt", `
             <iframe width="560" height="315"
-            src="https://www.youtube.com/embed/uY6e-dlTbdY"
+            src="https://www.youtube.com/embed/DrhoFDXpSi4"
             frameborder="0" allowfullscreen>
            </iframe>
           `)
@@ -81,7 +75,7 @@ Header(
         ,
         newText("<p></p>")
         ,
-        newButton("Iniciar")
+        newButton("INICIAR O TESTE")
         .log()
 )
 
@@ -111,16 +105,25 @@ Template("lista_de_pseudopalavras.csv",
         ,
         newText("N", row.Remover)
         ,
+        newText("Replay", "<u>OUVIR NOVAMENTE</u>")
+        .css("color", "blue")
+        ,
         
         newCanvas( "opcoes", 800, 200)
-            .add( "center at 40%" , "middle at 50%", getText("S") )
-            .add( "center at 60%" , "middle at 50%", getText("N") )
+            .add( "center at 20%" , "middle at 50%", getText("S") )
+            .add( "center at 50%" , "middle at 75%", getText("Replay") )
+            .add( "center at 80%" , "middle at 50%", getText("N") )
             .center()
             .print()
         ,
         
         newVar("RESPOSTA")
             .global()
+        ,
+        
+        newSelector("REPLAY")
+            .add( getText("Replay") )
+            .callback( getAudio("AudioExperiment").play() )
         ,
         
         newSelector("ESCOLHA")
@@ -144,17 +147,11 @@ Template("lista_de_pseudopalavras.csv",
 )
     
 newTrial( "Final" ,
-    newText("<p><strong>Obrigada pela participação!</strong></p>")
+    newText("<p style='text-align: justify'><strong>Agradecemos sinceramente a sua participação e o tempo dedicado à realização do teste. Sua colaboração é muito importante para nós. Para encerrar o teste clique no botão “FINALIZAR”.</strong></p>")
     .center()
     .print()
     ,
-    
-    newText("<p>Salve os dados da sua seleção clicando no botão <strong>Finalizar</strong>.</p>")
-    .center()
-    .print()
-    ,
-
-    newButton("Finalizar")
+    newButton("FINALIZAR")
     .wait()
  )
 
